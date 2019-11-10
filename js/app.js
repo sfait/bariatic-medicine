66abc19d3bdb2455fcb793a72d7e5e08170614f0
Sylwia Fait <sylwia.fait@gmail.com>
2019-11-11 00:12:08 +0100

    Add counter

--- js/app.js
+++ js/app.js
@@ -14,8 +14,27 @@ function showTooltip() {
     }
 }
 
+function countingDown() {
+    const daysCounter = document.querySelector(".promotion-days-counter");
+    const countDownDate = new Date("Dec 4, 2019 00:00:00").getTime();
+
+    const setinterval = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = countDownDate - now;
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+        daysCounter.textContent = days;
+
+        if (distance < 0) {
+            clearInterval(setinterval);
+            daysCounter.textContent = "0";
+        }
+    }, 1000);
+}
+
 const init = function() {
     showTooltip();
+    countingDown();
 }
 
 document.addEventListener("DOMContentLoaded", init);
